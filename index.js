b17e479265f9e0651e6b23b9b140a8afcda3a094
YAN FENG SHI <shiyanf@cn.ibm.com>
2018-03-29 15:42:49 +0800

    Clean code.

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 // Node module: ibmapm
 // This file is licensed under the Apache License 2.0.
 // License text available at https://opensource.org/licenses/Apache-2.0
-const util = require('util');
 
+const util = require('util');
 var log4js = require('log4js');
-var properties = require('properties');
-var fs = require('fs');
-var path = require('path');
+var commontools = require('./lib/tool/common');
+
 if (!global.NodeDCLoaded) {
     // The Node.js DC is not required.
     global.NodeDCLoaded = true;
@@ -18,8 +17,8 @@ if (!global.NodeDCLoaded) {
 var appmetrics = global.Appmetrics;
 // var configsvc = require('./lib/tool/configureservice').ConfigureService;
 
-//    initialize log
-if (process.env.KNJ_LOG_TO_CONSOLE) {
+// initialize log
+if (!commontools.testTrue(process.env.KNJ_LOG_TO_FILE)) {
     log4js.loadAppender('console');
 } else {
     log4js.loadAppender('file');
@@ -32,124 +31,28 @@ if (loglevel &&
     (loglevel === 'OFF' || loglevel === 'ERROR' || loglevel === 'INFO' ||
         loglevel === 'DEBUG' || loglevel === 'ALL')) {
     logger.setLevel(loglevel);
-    logger.info('KNJ_LOG_LEVEL is set to', loglevel);
 } else {
     logger.setLevel('INFO');
-    logger.info('KNJ_LOG_LEVEL is not set or not set correctly through environment variables.');
-    logger.info('The program set default log level to INFO.');
 }
-var commontools = require('./lib/tool/common');
 
 //    initialize log end
 // Sometimes we need to change the name of some environment variables to consistant all of DC.
 commontools.envDecrator();
 
-//    initialize different code path - BI/BAM/Agent
-var configObj;
-if (!process.env.MONITORING_SERVER_TYPE) {
-    try {
-        var configString = fs.readFileSync(path.join(__dirname,
-            '/etc/config.properties'));
+process.env.MONITORING_SERVER_TYPE = 'BAM';
 
-        configObj = properties.parse(configString.toString(), {
-            separators: '=',
-            comments: [';', '@', '#']
-        });
-        process.env.MONITORING_SERVER_TYPE = configObj.MONITORING_SERVER_TYPE;
-    } catch (e) {
-        logger.error('Failed to read etc/config.properties');
-        logger.error('Use default MONITORING_SERVER_TYPE: BAM');
-        logger.info(e);
-        process.env.MONITORING_SERVER_TYPE = 'BAM';
-    }
-}
-
-if (!process.env.MONITORING_SERVER_URL &&
-    configObj && configObj.MONITORING_SERVER_URL) {
-    process.env.MONITORING_SERVER_URL = configObj.MONITORING_SERVER_URL;
-}
-
-if (!process.env.MONITORING_APPLICATION_NAME &&
-    configObj && configObj.MONITORING_APPLICATION_NAME) {
-    process.env.MONITORING_APPLICATION_NAME = configObj.MONITORING_APPLICATION_NAME;
-}
-if (!process.env.MONITORING_SECURITY_URL &&
-    configObj && configObj.MONITORING_SECURITY_URL) {
-    process.env.MONITORING_SECURITY_URL = configObj.MONITORING_SECURITY_URL;
-}
-if (!process.env.MONITORING_SERVER_NAME &&
-    configObj && configObj.MONITORING_SERVER_NAME) {
-    process.env.MONITORING_SERVER_NAME = configObj.MONITORING_SERVER_NAME;
-}
 
 if (process.env.MONITORING_SECURITY_URL) {
     process.env.APM_KEYFILE_URL = process.env.MONITORING_SECURITY_URL;
 }
 
-// initialize shared configurations:
 if (commontools.testTrue(process.env.SECURITY_OFF)) {
     global.SECURITY_OFF = true;
 }
 
-if (typeof (process.env.KNJ_ENABLE_TT) === 'undefined' && configObj && configObj.KNJ_ENABLE_TT) {
-    process.env.KNJ_ENABLE_TT = configObj.KNJ_ENABLE_TT;
-}
-
-if (typeof (process.env.KNJ_SAMPLING) === 'undefined' && configObj && configObj.KNJ_SAMPLING) {
-    process.env.KNJ_SAMPLING = configObj.KNJ_SAMPLING;
-}
-
-if (typeof (process.env.KNJ_MIN_CLOCK_TRACE) === 'undefined' &&
-    configObj && configObj.KNJ_MIN_CLOCK_TRACE) {
-    process.env.KNJ_MIN_CLOCK_TRACE = configObj.KNJ_MIN_CLOCK_TRACE;
-}
-
-if (typeof (process.env.KNJ_MIN_CLOCK_STACK) === 'undefined' &&
-    configObj && configObj.KNJ_MIN_CLOCK_STACK) {
-    process.env.KNJ_MIN_CLOCK_STACK = configObj.KNJ_MIN_CLOCK_STACK;
-}
-
-if (typeof (process.env.KNJ_DISABLE_METHODTRACE) === 'undefined' &&
-    configObj && configObj.KNJ_DISABLE_METHODTRACE) {
-    process.env.KNJ_DISABLE_METHODTRACE = configObj.KNJ_DISABLE_METHODTRACE;
-}
-if (typeof (process.env.KNJ_AAR_BATCH_FREQ) === 'undefined' &&
-    configObj && configObj.KNJ_AAR_BATCH_FREQ) {
-    process.env.KNJ_AAR_BATCH_FREQ = configObj.KNJ_AAR_BATCH_FREQ;
-}
-if (typeof (process.env.KNJ_AAR_BATCH_COUNT) === 'undefined' &&
-    configObj && configObj.KNJ_AAR_BATCH_COUNT) {
-    process.env.KNJ_AAR_BATCH_COUNT = configObj.KNJ_AAR_BATCH_COUNT;
-}
-// initialize shared configurations end
-
 // initialize BAM configuration
-var bamConfObj;
 if (process.env.MONITORING_SERVER_TYPE === 'BAM') {
-    try {
-        var bamConfString = fs.readFileSync(path.join(__dirname,
-            '/etc/bam.properties'));
 
-        bamConfObj = properties.parse(bamConfString.toString(), {
-            separators: '=',
-            comments: [';', '@', '#']
-        });
-    } catch (e) {
-        logger.error('Failed to read etc/bam.properties.');
-        logger.error('Use default BAM configuration.');
-        logger.info(e);
-    }
-
-    if (bamConfObj) {
-        global.KNJ_AAR_BATCH_COUNT = process.env.KNJ_AAR_BATCH_COUNT ||
-            bamConfObj.KNJ_AAR_BATCH_COUNT;
-        global.KNJ_AAR_BATCH_FREQ = process.env.KNJ_AAR_BATCH_FREQ ||
-            bamConfObj.KNJ_AAR_BATCH_FREQ;
-        global.KNJ_ADR_BATCH_COUNT = process.env.KNJ_ADR_BATCH_COUNT ||
-            bamConfObj.KNJ_ADR_BATCH_COUNT;
-        global.KNJ_ADR_BATCH_FREQ = process.env.KNJ_ADR_BATCH_FREQ ||
-            bamConfObj.KNJ_ADR_BATCH_FREQ;
-    }
     global.KNJ_ADR_BATCH_COUNT = global.KNJ_ADR_BATCH_COUNT || 100;
     global.KNJ_ADR_BATCH_FREQ = global.KNJ_ADR_BATCH_FREQ || 60;
 
@@ -180,12 +83,12 @@ if (process.env.MONITORING_SERVER_TYPE === 'BAM') {
 // initialize BAM configuration end
 
 if (process.env.NODEJS_DC_DISABLE && process.env.NODEJS_DC_DISABLE.toLowerCase() === 'true') {
-    logger.fatal('The Node.js DC is disabled. ' +
+    logger.info('The Node.js DC is disabled. ' +
         ' Please refer to the document to configure the Node.js DC again.');
     return;
 }
 if (process.env.ITCAM_DC_ENABLED && process.env.ITCAM_DC_ENABLED.toLowerCase() === 'false') {
-    logger.fatal('The Node.js DC is disabled. ' +
+    logger.info('The Node.js DC is disabled. ' +
         ' Please refer to the document to configure the Node.js DC again.');
     return;
 }
